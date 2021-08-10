6df12ddf455ee2e996fec49ce493aa52fcef0d8e
n-ponto <noahpon@hotmail.com>
2021-08-09 20:00:09 -0700

    ManualStepFrame debugging

--- PyScripts/GUI/GUI.py
+++ PyScripts/GUI/GUI.py
@@ -1,9 +1,26 @@
-from PyScripts.GUI.ManualStepFrame import ManualStepFrame
 import tkinter as tk
 from Settings import Settings
+from SerialPort import SerialPort
+from ManualStepFrame import ManualStepFrame
+
+# Functions to run during closing
 
 root = tk.Tk()
-settingsMenu:tk.Frame = Settings(root)
+
+exit_functions: list = [root.quit]
+
+def exitScript():
+    for func in exit_functions:
+        func()
+
+root.protocol("WM_DELETE_WINDOW", exitScript)
+
+serial = SerialPort()
+settingsMenu:tk.Frame = Settings(root, serial)
 settingsMenu.grid(row=0, column=0)
+manualStepFrame = ManualStepFrame(root, serial, exit_functions)
+manualStepFrame.grid(row=0, column=1)
 root.mainloop()
+
+
 
--- PyScripts/GUI/ManualStepFrame.py
+++ PyScripts/GUI/ManualStepFrame.py
@@ -5,114 +5,135 @@ import time
 from threading import Thread
 
 directions =  {
-    "Up":         0b1000,
-    "Down":       0b0100,
-    "Left":       0b0010,
-    "Right":      0b0001
+    "up":         0b1000,
+    "down":       0b0100,
+    "left":       0b0010,
+    "right":      0b0001,
+    "w":         0b1000,
+    "s":       0b0100,
+    "a":       0b0010,
+    "d":      0b0001
     }
+
+PEN_UP = 0b1100
+PEN_DOWN = 0b0011
 
 curDirection: int = 0
 
 serial: SerialPort
-running: bool = False
+running_flag: bool = False
+pen_flag: bool = True
+penIsUp: bool = True
 
 timeoutSeconds = 10
 
+# Function designed to be run in it's own thread so it can send continuous instructions
+# to the arduino without handling user input directly
 def writeDirection():
     print("Starting")
-    # Send the command code for manual stepping
-    serial.writeByte(5)
+    serial.writeByte(5) # Send the command code for manual stepping
+    serial.writeByte(PEN_UP) # Send code to initilize the pen in the up position
     time.sleep(0.5)
     serial.read()
     lastActive = time.time()
-    global running
+    global running_flag, pen_flag, penIsUp
     while(True):
-        if (running):
-            if curDirection:
-                pass
+        if (running_flag):
+            global pen_flag
+            if pen_flag:
+                if penIsUp:
+                    penIsUp = False
+                    serial.writeByte(PEN_DOWN)
+                else:
+                    penIsUp = True
+                    serial.writeByte(PEN_UP)
+                pen_flag = False
+            elif curDirection:
                 # print(curDirection)
                 serial.writeByte(curDirection)
             elif (time.time()-lastActive > timeoutSeconds):
-                running = True
+                print("Timeout")
+                running_flag = False
                 break
+            lastActive = time.time()
         else:
             break
-    
-    print("Ending")
-    serial.writeByte(0b0000)
+    serial.writeByte(0b0000) # Null byte exits from manual step mode
     time.sleep(0.1)
     serial.read()
+    print("Manual Step Thread Finished") 
 
-def handle_keypress(event):
-        print(event)
-        sym = event.keysym
-        direction = directions[sym]
-        global curDirection
-        if sym in directions and not curDirection & direction: # any([(curDirection & direction) for x in directions.values()]):
-            curDirection = curDirection | direction
-            print (curDirection)
+# Should be called on exit to ensure the thread is finished
+def exit():
+    global running_flag
+    if running_flag:
+        print("Ending Manual Step Thread...")
+        running_flag = False
 
+# Creates the manual step frame
 class ManualStepFrame(tk.Frame):
 
     serial: SerialPort
     thread: Thread
 
-    def __init__(self, master: tk.Misc, sp: SerialPort):
+    def __init__(self, master: tk.Misc, sp: SerialPort, exit_functions: list):
         super().__init__(master=master, width=200, height=200)
         self.bind("<KeyPress>", self.handle_keypress)
         self.bind("<KeyRelease>", self.handle_keyrelease)
         self.bind("<Button-1>", self.handle_click)
         self.bind("<FocusOut>", self.handle_foucus_out)
-        # lbl_header = tk.Label(master=self, text="Manual Step")
-        # lbl_header.pack()
-        global running, serial
+        exit_functions.append(exit)
+        global running_flag, serial
         serial = sp
     
-
-    def getFrame(self, master: tk.Misc) -> tk.Frame:
-        print("Getting frame")
-        
-        return self.frame
-    
     def handle_keypress(self, event):
-        print(event)
-        sym = event.keysym
-        direction = directions[sym]
-        global curDirection
-        if sym in directions and not curDirection & direction:
-            curDirection = curDirection | direction
+        sym: str = event.keysym.lower()
+        global curDirection, pen_flag
+        if sym in directions and not curDirection & directions[sym]:
+            curDirection = curDirection | directions[sym]
             print (curDirection)
+        elif sym == 'space' and penIsUp:
+            print("Pen Down")
+            pen_flag = True        
 
     def handle_keyrelease(self, event):
-        sym = event.keysym
+        sym = event.keysym.lower()
+        global pen_flag, curDirection
         if sym in directions:
-            global curDirection
             curDirection = curDirection & ~directions[sym]
+        elif sym == 'space' and not penIsUp:
+            print("Pen Up")
+            pen_flag = True
 
     def handle_click(self, event):
         self.focus_set()
         self.config(bg="red")
         print(event)
-        global running
-        if not running:
-            running = True
+        global running_flag
+        if not running_flag:
+            running_flag = True
             self.thread = Thread(target=writeDirection)
             self.thread.start()
     
     def handle_foucus_out(self, event=None):
         print("Focus lost")
         self.config(bg='white')
-        global running; running = False
+        global running_flag; running_flag = False
+
+
+
+################################################################################
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Settings Menu")
     sp = SerialPort()
-    manualStepFrame = ManualStepFrame(root, sp)
+    manualStepFrame = ManualStepFrame(root, sp, list())
     manualStepFrame.focus_set()
     manualStepFrame.pack()
     sp.awaitResponse()
-    def exit(): manualStepFrame.handle_foucus_out(); root.quit()
-    root.protocol("WM_DELETE_WINDOW", exit)
+    def dev_exit(): exit(); root.quit()
+    root.protocol("WM_DELETE_WINDOW", dev_exit)
     root.mainloop()
     
